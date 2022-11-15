df25e9c26257361e00518ece0cdb33053964ae1f
Iara Pou <iarapou21@hotmail.com>
2022-11-15 19:30:20 -0300

    Arregla bug
    Noté que pasar el atributo disabled no deshabilitaba a los cuadrados, sino que antes no funcionaban porque nunca les había asignado un onclick.
    Entonces, cree una función específica que solo sirva para deshabilitar el botón y otra que sirva para deshabilitar los cuadrados.

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const secuenciaMaquina = [];
 let secuenciaUsuario = [];
 let contadorRondas = 0;
 
-function deshabilitar (elemento){
+function deshabilitarBoton (elemento){
     elemento.disabled = true;
 }
 
 function deshabilitarCuadrados (){
-    document.querySelectorAll(".cuadrado").forEach($cuadrado => deshabilitar($cuadrado));
+    const $cuadrados = document.querySelectorAll(".cuadrado");
+    $cuadrados.forEach($cuadrado => $cuadrado.onclick = "");
 }
 
 function devolverCuadradoRandom(){
@@ -21,9 +22,8 @@ function devolverCuadradoRandom(){
 }
 
 function manejarRonda (){
-    deshabilitar ($botonEmpezar);
+    deshabilitarBoton();
     deshabilitarCuadrados();
-    
     secuenciaMaquina.push(devolverCuadradoRandom());
     const RETRASO_TURNO_JUGADOR = (secuenciaMaquina.length + 1)*1000;
 
